ecc5d1a5e81eee4bf430b7e70a989264ebe6818e
Muhes Ariyaratnam <47985390+muhes@users.noreply.github.com>
2020-12-21 01:22:17 -0500

    added comparison function

--- nba-api.py
+++ nba-api.py
@@ -16,3 +16,23 @@ from nba_api.stats.endpoints import playergamelog
 
 gamelog_bron = playergamelog.PlayerGameLog(player_id = '2544', season = '2019')
 gamelog_bron_df = gamelog_bron.get_data_frames()[0]
+sum = gamelog_bron_df.sum()
+
+def getPlayerId(full_name):
+    player = [player for player in player_dict if player['full_name'] == full_name][0]
+    return player['id']
+
+def comparePlayers(p1_full_name, p2_full_name):
+    p1_id = getPlayerId(p1_full_name)
+    p2_id = getPlayerId(p2_full_name)
+    p1_gamelog = playergamelog.PlayerGameLog(player_id = p1_id, season = '2019')
+    p1_df = p1_gamelog.get_data_frames()[0]
+    p2_gamelog = playergamelog.PlayerGameLog(player_id = p2_id, season = '2019')
+    p2_df = p2_gamelog.get_data_frames()[0]
+    p1_sum = p1_df.sum()
+    p2_sum = p2_df.sum()
+    return p2_sum['PTS']
+
+
+    
+    
