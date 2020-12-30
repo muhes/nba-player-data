ad9d3ebea4711276aadeec7ec8d6629346f0b167
Muhes Ariyaratnam <47985390+muhes@users.noreply.github.com>
2020-12-30 00:38:23 -0500

    updated formatting for comparison table

--- nba-api.py
+++ nba-api.py
@@ -14,8 +14,6 @@ from nba_api.stats.endpoints import playergamelog
 
 def __getPlayerId(full_name):
     player = [player for player in player_dict if player['full_name'] == full_name]
-    print(player)
-    #if len(player) != 0:
     try:
         player = player[0]
         return player['id']
@@ -26,7 +24,7 @@ def __getPlayerId(full_name):
 # this function grabs only the necesary stats (9 categories) for each player with the id
 def __get9CatStats(player_name, type):
     p_id = __getPlayerId(player_name)
-    p_gamelog = playergamelog.PlayerGameLog(player_id = id, season = '2019')
+    p_gamelog = playergamelog.PlayerGameLog(player_id = p_id, season = '2020' )
     p_df = p_gamelog.get_data_frames()[0]
     if type == 'total':
     # this line extract all of the necessary counting stats from the sum
@@ -50,12 +48,26 @@ def __addPlayerToList(player):
         team_df[player] = player_9cat
     
 
-def tradeEvaluater(team1_list, team2_list, type):
-    team1_df = {};
+def tradeEvaluator(team1_list, team2_list, type):
+    #FINISH
+    team1_df = []
+    team2_df = []
     for player in team1_list:
-        __addPlayerToList(player)
+        print('yio')
+        if team1_df.empty:
+            team1_df = __get9CatStats(player, type)
+        else:
+            p_df = __get9CatStats(player, type)
+            team1_df = team1_df.add(p_df)
+    for player in team2_list:
+        if team2_df.empty:
+            team2_df = __get9CatStats(player, type)
+        else:
+            p_df = __get9CatStats(player, type)
+            team2_df = team1_df.add(p_df)
+    return team1_df
         
-    
+
         
     
     #returns totals for all players in trade
@@ -78,9 +90,20 @@ def comparePlayers(p1_full_name, p2_full_name, type):
     p1_9cat = p1_9cat.to_frame()
     p2_9cat = p2_9cat.to_frame()
     p1_9cat[p2_full_name] = p2_9cat[0]
-    p1_9cat['comparison'] = np.where(p1_9cat[0] > p1_9cat[p2_full_name], 'True', 'False')
+    p1_9cat['Better Player'] = np.where(p1_9cat[0] > p1_9cat[p2_full_name], p1_full_name, p2_full_name)
+    #p1_9cat.rename(index={0: p1_full_name})
+    p1_9cat.columns = [p1_full_name if x==0 else x for x in p1_9cat.columns]
     return p1_9cat
 
+ayton = __get9CatStats('Deandre Ayton', 'average')
+buddy = __get9CatStats('Buddy Hield', 'average')
+
+team2 = buddy.add(ayton)
+#print(team2)
+bron = __get9CatStats('LeBron James', 'average')
+cap = __get9CatStats('Clint Capela', 'average')
+team1 = bron.add(cap)
+#print(team1)
 
 
     
